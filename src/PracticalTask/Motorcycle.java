09f0e3b27255727ea249a4c1b1963ccd10c45e17
Irena Markov <i.markov@vegait.rs>
2023-03-17 11:02:24 +0100

    PracticalTask

--- src/PracticalTask/Motorcycle.java
+++ src/PracticalTask/Motorcycle.java
@@ -9,30 +9,34 @@ public class Motorcycle extends Vehicle{
     @Override
     public double calculateRegistrationCost() {
         int registrationPrice = 0;
+        int currentYear = LocalDate.now().getYear();
+        int motorcycleAge = currentYear - getProductionYear();
 
-        if (getEngineSize() < 50){
-            int currentYear = LocalDate.now().getYear();
-            int motorcycleAge = currentYear - getProductionYear();
-            if (motorcycleAge < 1){
-                registrationPrice = 5000;
-            }
-        } else {
+        if (getEngineSize() < 50 && motorcycleAge < 1) {
+            registrationPrice = 5000;
+        } else if (getEngineSize() < 50 && motorcycleAge > 1) {
             registrationPrice = 1000;
         }
-        return registrationPrice;
-    }
-    @Override
-    public String toString() {
-        return "Motorcycle : " + getBrand() + "\n" +
-                "Engine Size : " + getEngineSize() + " cc\n" +
-                "Horse Powers : " + getHorsePowers() + " hp\n" +
-                "Engine Type : " + getEngineType() + "\n" +
-                "Production Year : " + getProductionYear() + "\n" +
-                "REGISTRATION AMOUNT : " + calculateRegistrationCost() + "\n";
-    }
+            else if (getEngineSize() >= 50) {
+                registrationPrice = 5000;
+            } else {
+                registrationPrice = 1000;
+            }
+            return registrationPrice;
+        }
 
+        @Override
+        public String toString () {
+            return "Motorcycle : " + getBrand() + "\n" +
+                    "Engine Size : " + getEngineSize() + " cc\n" +
+                    "Horse Powers : " + getHorsePowers() + " hp\n" +
+                    "Engine Type : " + getEngineType() + "\n" +
+                    "Production Year : " + getProductionYear() + "\n" +
+                    "REGISTRATION AMOUNT : " + calculateRegistrationCost() + "\n";
+        }
 
 }
+
 
 
 
